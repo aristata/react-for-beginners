efe45e981c7bd5b613fdd59f917e4cfc10777711
장성민 <earnestgem@optatumplatform.com>
2022-05-09 17:50:47 +0900

    effect 소개

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Button from "./Button";
 import styles from "./App.module.css";
+import {useState} from "react";
 
 function App() {
+    const [counter, setCounter] = useState(0);
+    const onClickHandler = () => setCounter((prev) => prev + 1);
+    console.log("api call ...");
     return (
         <div>
-            <h1 className={styles.title}>Welcome back!</h1>
-            <Button text={"Continue"}/>
+            <h1 className={styles.title}>{counter}</h1>
+            <button onClick={onClickHandler}>Click me !!!</button>
         </div>
     );
 }
