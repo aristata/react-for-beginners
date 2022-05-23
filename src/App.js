67e5f1e92e468ec9190bb729805ebbbb3648a619
장성민 <earnestgem@optatumplatform.com>
2022-05-23 17:51:49 +0900

    #7.0 ToDo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import {useState} from "react";
+
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    console.log(toDo);
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
   return (
     <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input type={"text"} placeholder={"Write your to do..."} value={toDo} onChange={onChange}/>
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
