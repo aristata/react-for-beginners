cd7c523b6ab5a524b9d9336a5c294a4b9cd48fb0
장성민 <earnestgem@optatumplatform.com>
2022-05-27 16:20:18 +0900

    7.2 coin tracker

--- src/App.js
+++ src/App.js
@@ -1,31 +1,68 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [selectedCoin, setSelectedCoin] = useState({});
+  const [budget, setBudget] = useState(0);
+  const [amountOfCoins, setAmountOfCoins] = useState("0");
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        setCoins(json.slice(0, 20));
+        setLoading(false);
+      });
+  }, []); // 아무것도 주시하고 있지 않으면 (= 빈 배열이면) 이 코드는 한 번만 작동한다
+  
+  const handleSelect = (e) => {
+    const selectedCoin = coins[e.target.value];
+    setSelectedCoin(selectedCoin);
+  }
+  
+  const handleInput = (e) => {
+    setBudget(parseFloat(e.target.value));
+  }
+  
+  useEffect(() => {
+    if (!!selectedCoin && !!budget) {
+      const coinPrice = selectedCoin.quotes.USD.price;
+      const amountOfCoinsYouCanBuy = (budget / coinPrice).toFixed(4);
+      setAmountOfCoins(amountOfCoinsYouCanBuy);
     }
-    console.log(toDo);
-    setToDos((currentArray) => [...currentArray, toDo]);
-    setToDo("");
-  };
+  }, [selectedCoin, budget])
+  
   return (
     <>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input type={"text"} placeholder={"Write your to do..."} value={toDo} onChange={onChange}/>
-        <button>Add To Do</button>
-      </form>
-      <hr/>
-      <ul>
-        {toDos.map((toDo, index) => {
-          return <li key={`toDo ${index}`}>{toDo}</li>
-        })}
-      </ul>
+      <div>
+        <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
+        {loading ? (
+          <strong>Loading...</strong>
+        ) : (
+          <div>
+            <select onChange={handleSelect}>
+              <option key={"default"}>select coin!</option>
+              {coins.map((coin, index) => {
+                return (
+                  <option key={index} value={index}>
+                    {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+                  </option>
+                )
+              })}
+            </select>
+            <div>
+              <label htmlFor={"moneyInput"}>My Budget: </label>
+              <input id={"moneyInput"} type={"number"} onChange={handleInput} value={budget}/>
+              <span> USD</span>
+            </div>
+            <div>
+              <span>You can buy: {amountOfCoins} {selectedCoin.symbol}</span>
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 }
